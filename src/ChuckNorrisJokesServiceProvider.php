ba71ddeb2403335d1375f43009015e7912b3f40c
E J Del Monico <ejdelmonico@gmail.com>
2019-05-27 10:09:22 -0700

    Add Laravel facade and service provider

---
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ejdelmonico\ChuckNorrisJokes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChuckNorrisJokesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    { }
+
+    public function register()
+    {
+        $this->app->bind('chuck-norris', function () {
+            return new JokeFactory();
+        });
+    }
+}
